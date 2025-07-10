0082368f631fb7e28b12f3688ba23fb869fed279
Athran Rahman <cengallut@gmail.com>
2025-07-10 17:31:55 +0800

    fix annotation

--- codecraftbit.ts
+++ codecraftbit.ts
@@ -4,32 +4,29 @@
 //% color=#bb1792 block="Codecraft:bit"
 //% groups="['DC Motor', 'Servo']"
 namespace codecraftbit {
+  //% subcategory=Sensors
   //% block
   export function foo() {
     console.log("hello world");
   }
 
   //% subcategory=Sensors
-  //% blockId=codecraftbit-sensor-read
   //% block
   export function readSensor(): number {
     return 42;
   }
 
   //% subcategory=Actuators
-  //% blockId=codecraftbit-actuator-servo-set
   //% block
   //% group="Servo"
   export function setServo(pos: number) {}
 
   //% subcategory=Actuators
-  //% blockId=codecraftbit-actuator-dcmotor-spin
   //% block
   //% group="DC Motor"
   export function spinMotor(pos: number) {}
 
   //% subcategory=Actuators
-  //% blockId=codecraftbit-actuator-dcmotor-stop
   //% block
   //% group="DC Motor"
   export function stopMotor(pos: number) {}
