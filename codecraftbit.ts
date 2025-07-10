edd109abb0b57c1bf89b5f6fac6ecc0c9b213d6f
Athran Rahman <cengallut@gmail.com>
2025-07-10 17:19:51 +0800

    add block subcategories and groups

--- codecraftbit.ts
+++ codecraftbit.ts
@@ -1,10 +1,32 @@
 /**
  * Codecraft:bit blocks
  */
-//% color=#00ff00 block="Codecraft:bit"
+//% color=#bb1792 block="Codecraft:bit"
+//% groups="['DC Motor', 'Servo']"
 namespace codecraftbit {
   //% block
   export function foo() {
     console.log("hello world");
   }
+
+  //% subcategory=Sensors
+  //% blockId=codecraftbit-sensor-read
+  export function readSensor(): number {
+    return 42;
+  }
+
+  //% subcategory=Actuators
+  //% group="Servo"
+  //% blockId=codecraftbit-actuator-servo-set
+  export function setServo(pos: number) {}
+
+  //% subcategory=Actuators
+  //% group="DC Motor"
+  //% blockId=codecraftbit-actuator-dcmotor-spin
+  export function spinMotor(pos: number) {}
+
+  //% subcategory=Actuators
+  //% group="DC Motor"
+  //% blockId=codecraftbit-actuator-dcmotor-stop
+  export function stopMotor(pos: number) {}
 }
